5565ced9379055f96c4a5bcb7309ba52ba10dd48
kochergin-alex32 <kochergin.a.v@bk.ru>
2023-10-01 23:48:06 +0300

    сделал ранодом по Y

--- src/components/Canvas/useCanvas.js
+++ src/components/Canvas/useCanvas.js
@@ -10,23 +10,47 @@ const useCanvas = draw => {
     useEffect(()=>{
         const canvas = ref.current;
         const context = canvas.getContext('2d')
+
+        // let offsetY = Math.random()*280+50
+        // let freq = Math.random()*0.01
+        // let ampl = Math.random()*90+40
+        // let a2 = offsetY + ampl *  Math.cos(count2 * freq)
+
+
         let count =0
-        let a =Math.floor( Math.random()*400+10)
-  
-        // console.log(count);
+        let count2 = 0
+        let y =Math.floor( Math.random()*400+10)
+        let y2 = Math.floor( Math.random()*400+10)
+
+       
+
         let animationID
 
         const render = ()=>{
            
-            // let a = offsetY + ampl *  Math.cos(count * freq)
-            
-            count+=Math.random()*4+0.5
+            count+=1
+            count2+=1.1
+          
+            // добавил проверку чтобы рыбы каждый раз по рандомно выплывали 
+            if(count>=795){
+                count = 0
+                console.log('hello');
+                y=Math.floor( Math.random()*400+10)
+                
+              }
+              if(count2>=795){
+                count2 = 0
+                y2= Math.floor( Math.random()*400+10)
+              
+               
+              }
                
             
-           console.log(a);
-            draw(context,count,a)
+          
+            draw(context,count,count2,y,y2)
             animationID= window.requestAnimationFrame(render)
         }
+       
        render()
        return () =>window.cancelAnimationFrame(animationID)
     },[draw])
--- src/components/GameArea/GameArea.js
+++ src/components/GameArea/GameArea.js
@@ -3,13 +3,40 @@ import './GameArea.css'
 import Canvas from '../Canvas/Canvas'
 const GameArea = () => {
 
-    const draw = (context,count,a)=>{
-      
-        const am = a % 500
+    const draw = (context,count,count2,y,y2)=>{
+        
+    
+
+        const am = y%500
+        const am1 = y2%500
         const delta1 = count % 800 
-        console.log(am);
-        context.clearRect(0,0,context.canvas.width,context.canvas.height)
-        context.fillStyle = 'grey'
+        const delta = count2
+        
+       const redFish = ()=>{context.fillStyle = 'red'
+       context.beginPath()
+       context.arc(800-delta, am1, 10, 0, 2*Math.PI, false) 
+       context.fill()
+       context.stroke()
+        context.beginPath()
+       context.arc((800+20)-delta, am1, 10, Math.PI/2, 3*Math.PI/2, false)
+       context.fill()
+       context.stroke()
+       context.fillStyle = "white"
+        context.beginPath()
+        context.arc((800-5)-delta, am1-3, 3, 0, 2*Math.PI, false)
+        context.fill()
+        context.fillStyle = "black"
+       context.beginPath()
+       context.arc((800-5)-delta, am1-3, 1, 0, 2*Math.PI, false)
+       context.fill()
+       context.fillStroke = "black"
+        context.beginPath()
+      context.moveTo((800 - 9)-delta, am1+4)
+         context.lineTo((800 - 4)-delta,am1+4)
+         context.stroke()}
+ 
+        const greyFish =()=>{
+          context.fillStyle = 'grey'
         // метод говорит что мы начинаем отрисовку
         context.beginPath()
         context.arc(800-delta1, am, 10, 0, 2*Math.PI, false)
@@ -37,16 +64,32 @@ const GameArea = () => {
        context.moveTo((800 - 9)-delta1, am+4)
           context.lineTo((800 - 4)-delta1,am+4)
           context.stroke()
+        }
+        
+
+        context.clearRect(0,0,context.canvas.width,context.canvas.height)
+     
+        greyFish()
+        redFish()
+
+   
        
 
     }
-    const draw2 = (context,count)=>{
-        context.clearRect(0,0,context.canvas.width,context.canvas.height)
-        context.fillStyle = 'grey'
-        const delta = count % 500
-        context.fillRect(10,500-delta,100,100)
+ 
+ 
+    // const draw2 = (context,count)=>{
+    //     context.clearRect(0,0,context.canvas.width,context.canvas.height)
+    //     context.fillStyle = 'grey'
+    //     const delta = count % 500
+    //     context.fillRect(10,500-delta,100,100)
+ 
+    //     // context.clearRect(0,0,context.canvas.width,context.canvas.height)
+    //     context.fillStyle = 'grey'
+       
+    //     context.fillRect(10,0+delta,100,100)
 
-    }
+    // }
 
   return (
     <>
@@ -59,3 +102,60 @@ const GameArea = () => {
 }
 
 export default GameArea
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //   context.fillStyle = 'grey'
+      //   // метод говорит что мы начинаем отрисовку
+      //   context.beginPath()
+      //   context.arc(800-delta1, am, 10, 0, 2*Math.PI, false)
+      //   // заливаем окружность нашим цветом
+      //   context.fill()
+      //   context.stroke()
+      //    // создаем хвост
+      //    context.beginPath()
+      //   context.arc((800+20)-delta1, am, 10, Math.PI/2, 3*Math.PI/2, false)
+      //   context.fill()
+      //   context.stroke()
+      //    // рисуем глаз
+      //   context.fillStyle = "white"
+      //    context.beginPath()
+      //    context.arc((800-5)-delta1, am-3, 3, 0, 2*Math.PI, false)
+      //    context.fill()
+      //    // рисуем зрачек
+      //    context.fillStyle = "black"
+      //   context.beginPath()
+      //   context.arc((800-5)-delta1, am-3, 1, 0, 2*Math.PI, false)
+      //   context.fill()
+      //   // рот
+      //   context.fillStroke = "black"
+      //    context.beginPath()
+      //  context.moveTo((800 - 9)-delta1, am+4)
+      //     context.lineTo((800 - 4)-delta1,am+4)
+      //     context.stroke()
